bbadc7e8d3c400ed3c0aef32e55006e769f4a6ba
unknown <guilhermi_silva@hotmail.com>
2025-11-08 14:08:07 -0300

    ajustado config app module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // Adicione esta linha
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
