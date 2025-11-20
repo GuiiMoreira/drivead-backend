2e3164490beb1a5e654eafeb9d4c9feaff1bb07a
Guilherme Moreira <88728097+GuiiMoreira@users.noreply.github.com>
2025-11-20 10:57:49 -0300

    Update campaigns.module.ts

--- src/campaigns/campaigns.module.ts
+++ src/campaigns/campaigns.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { CampaignsService } from './campaigns.service';
 import { CampaignsController } from './campaigns.controller';
 import { PaymentsModule } from '../payments/payments.module';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [
+    PaymentsModule,
+    StorageModule
+  ],
+  controllers: [CampaignsController],
   providers: [CampaignsService],
-  controllers: [CampaignsController]
 })
-export class CampaignsModule { }
+export class CampaignsModule {}
