87bea907eafbdd0c3a4a6cfcb6ce80a1761952f2
Guilherme Moreira <88728097+GuiiMoreira@users.noreply.github.com>
2026-03-13 18:25:57 +0000

    fix: importado StorageModule no advertisers module

--- src/advertisers/advertisers.module.ts
+++ src/advertisers/advertisers.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AdvertisersController } from './advertisers.controller';
 import { AdvertisersService } from './advertisers.service';
-
+import { AdvertisersController } from './advertisers.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { StorageModule } from '../storage/storage.module';
 @Module({
+  imports: [
+    PrismaModule,
+    StorageModule
+  ],
   controllers: [AdvertisersController],
-  providers: [AdvertisersService]
+  providers: [AdvertisersService],
 })
 export class AdvertisersModule {}
